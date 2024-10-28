532d03ed50c9e98ed35100b007de41312280e59a
cataC20 <103144344+cataC20@users.noreply.github.com>
2024-10-28 01:51:20 -0300

    Añadida configuración de Firebase y correcciones de bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyC-R54B6nAyrSHwArhymM8CBTEFWbT4jU0",
+    authDomain: "thanosapp-e38e5.firebaseapp.com",
+    projectId: "thanosapp-e38e5",
+    storageBucket: "thanosapp-e38e5.appspot.com",
+    messagingSenderId: "426170863669",
+    appId: "1:426170863669:web:71667be7578b9e261b617c",
+    measurementId: "G-NY2Y8Q0DKW"
+  },
 };
